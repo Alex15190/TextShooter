572b9da86544f6fff4d8b7117896686e36bb56ce
Alex Chekodanov <checkodanov@gmail.com>
2018-08-31 15:05:58 +0300

    Initial Commit

---
+++ TextShooter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextShooter
+//
+//  Created by Alex Chekodanov on 31.08.2018.
+//  Copyright © 2018 MERA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TextShooter/GameViewController.h
@@ -0,0 +1,15 @@
+//
+//  GameViewController.h
+//  TextShooter
+//
+//  Created by Alex Chekodanov on 31.08.2018.
+//  Copyright © 2018 MERA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+#import <GameplayKit/GameplayKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
